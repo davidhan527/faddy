febd22ca8af6e1cf5634e6e5af7bc504d267aa04
davidhan527 <davidhan527@gmail.com>
2013-09-23 16:19:32 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faddy::Application.config.secret_token = '1352a3c9096f6779035348ac00a1b386fcd97e53685c9a1aabcb78bc422469a0631b071b044697abe3195e74efb290072d2f4888b333159f56b05c95a29029ba'
