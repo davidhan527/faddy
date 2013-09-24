6d1f44449ef87fa5617e6cf5dfed817a0a0a9fb8
davidhan527 <davidhan527@gmail.com>
2013-09-24 17:15:35 -0400

    editing error

--- app/controllers/faddys_controller.rb
+++ app/controllers/faddys_controller.rb
@@ -2,13 +2,10 @@ class FaddysController < ApplicationController
 
   def index
     client = Instagram.client(:access_token => session[:access_token])
-      user = client.user
+      @user = client.user
 
-    html = "<h1>#{user.username}'s recent photos</h1>"
-  for media_item in client.user_recent_media
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
+    
   end
-  html
-  end
+
 
 end
