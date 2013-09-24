2c97623f704e9bfabe5214723a006502b2e7a3e9
davidhan527 <davidhan527@gmail.com>
2013-09-24 16:00:49 -0400

    adding files

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,8 @@
+require "instagram"
+
+Instagram.configure do |config|
+  config.client_id = "7ffb6e89a4e74a23b0dc42b0ad6a6588"
+  config.client_secret = "1d9bcb7f338c490f8136cdfa133b5bbd"
+end
+
+CALLBACK_URL = "http://hidden-hamlet-7511.herokuapp.com/"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,35 @@
 Faddy::Application.routes.draw do
 
-root :to => 'faddy#index'
+  match 'user/:username', :to => 'users#index'
+  match 'session/:action', :to => 'sessions'
+  root :to => "sessions#connect"
 
+# root :to => 'faddys#index'
+
+# get '/faddys', :to => 'faddys#connect'
+
+# post '/faddys', :to => 'faddys#callback'
+
+# get "/oauth/connect" do
+#   redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+# end
+
+# get "/oauth/callback" do
+#   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+#   session[:access_token] = response.access_token
+#   redirect_to root_path
+# end
+
+# get "/feed" do
+#   client = Instagram.client(:access_token => session[:access_token])
+#   user = client.user
+
+#   html = "<h1>#{user.username}'s recent photos</h1>"
+#   for media_item in client.user_recent_media
+#     html << "<img src='#{media_item.images.thumbnail.url}'>"
+#   end
+#   html
+# end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
