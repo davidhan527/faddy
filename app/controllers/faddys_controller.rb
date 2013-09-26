a07045c11aec8e9384618536901fb20bb103347b
davidhan527 <davidhan527@gmail.com>
2013-09-25 20:27:53 -0400

    adding left right coordinates in addition to top and bottom and lower result counts to 60

--- app/controllers/faddys_controller.rb
+++ app/controllers/faddys_controller.rb
@@ -20,13 +20,15 @@ class FaddysController < ApplicationController
     x = coordinates[0]
     y = coordinates[1]
 
-    loc = Instagram.media_search("#{x}","#{y}", options = {:count => 100})
-    loc_top = Instagram.media_search("#{x + 0.006}","#{y}", options = {:count => 100})
-    loc_bottom = Instagram.media_search("#{x - 0.006}","#{y}", options = {:count => 100})
+    loc = Instagram.media_search("#{x}","#{y}", options = {:count => 60})
+    loc_top = Instagram.media_search("#{x + 0.006}","#{y}", options = {:count => 60})
+    loc_bottom = Instagram.media_search("#{x - 0.006}","#{y}", options = {:count => 60})
+    loc_right = Instagram.media_search("#{x}","#{y - 0.06}", options = {:count => 60})
+    loc_left = Instagram.media_search("#{x}","#{y + 0.06}", options = {:count => 60})
 
-    results = loc + loc_top + loc_bottom
-
-    # binding.pry
+    results = loc + loc_top + loc_bottom + loc_left + loc_right
+    
+    binding.pry
     # page_1 = Instagram.media_search(x, y)
     # page_2_max_id = page_1.pagination.next_max_id
     # page_2 = Instagram.media_search(x, y, :max_id => page_2_max_id ) unless page_2_max_id.nil?
