f662ba1c7e73284cd01697b0f9b72a20f2740d49
davidhan527 <davidhan527@gmail.com>
2013-11-18 14:56:44 -0500

    adding readme

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Faddy::Application.routes.draw do
   get '/location', :to => 'faddys#new'
 
   get '/food', :to => 'faddys#food'
-
+ 
   get '/desserts', :to => 'faddys#desserts'
 
   get '/drinks', :to => 'faddys#drinks'
