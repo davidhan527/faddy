d1e30ed14f9e81f3830927f7590d1d2dc27c0beb
davidhan527 <davidhan527@gmail.com>
2013-12-01 15:49:23 -0500

    find results method

--- app/controllers/faddys_controller.rb
+++ app/controllers/faddys_controller.rb
@@ -11,23 +11,11 @@ class FaddysController < ApplicationController
       @tag_filter = ["dog", "sexy", "fashion", "swag", "funny", "gay"]
       @which_num = 2
     end
-    
 
     def desserts
       client = Instagram.client(:access_token => session[:access_token])      
       location = params[:location]
-      coordinates = Geocoder.coordinates("#{location}")
-    
-      x = coordinates[0]
-      y = coordinates[1]
-
-      loc = Instagram.media_search("#{x}","#{y}", options = {:count => 100})
-      loc_top = Instagram.media_search("#{x + 0.012}","#{y}", options = {:count => 100})
-      loc_bottom = Instagram.media_search("#{x - 0.012}","#{y}", options = {:count => 100})
-      loc_right = Instagram.media_search("#{x}","#{y - 0.012}", options = {:count => 100})
-      loc_left = Instagram.media_search("#{x}","#{y + 0.012}", options = {:count => 100})
-
-      results = loc + loc_top + loc_bottom + loc_left + loc_right
+      find_results(location)
 
       @desserts_sorted = results.sort_by {|hash| hash.likes["count"]}.uniq.reverse! #first(15)
       @desserts_tags = ["dessert", "desserts", "sweet", "chocolate", "cake", "icecream", "dessertporn", "delish", "sweettooth"]
@@ -37,18 +25,8 @@ class FaddysController < ApplicationController
     def drinks
       client = Instagram.client(:access_token => session[:access_token])      
       location = params[:location]
-      coordinates = Geocoder.coordinates("#{location}")
-    
-      x = coordinates[0]
-      y = coordinates[1]
-
-      loc = Instagram.media_search("#{x}","#{y}", options = {:count => 100})
-      loc_top = Instagram.media_search("#{x + 0.012}","#{y}", options = {:count => 100})
-      loc_bottom = Instagram.media_search("#{x - 0.012}","#{y}", options = {:count => 100})
-      loc_right = Instagram.media_search("#{x}","#{y - 0.012}", options = {:count => 100})
-      loc_left = Instagram.media_search("#{x}","#{y + 0.012}", options = {:count => 100})
-
-      results = loc + loc_top + loc_bottom + loc_left + loc_right
+      find_results(location)
+      
       @drinks_sorted = results.sort_by {|hash| hash.likes["count"]}.uniq.reverse! #first(15)
       @drinks_tags = ["drink", "drinks", "slurp", "pub", "bar", "liquor", "thirst", "thirsty", "cocktail", "cocktails", "drinkup", "glass", "beer", "beers", "wine"]
       @tag_filter = ["dog", "sexy", "fashion", "swag", "funny", "gay"]
