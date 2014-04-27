a868b2e5af57b586f99348c9b81c094d5fb1ba70
davidhan527 <davidhan527@gmail.com>
2014-04-27 16:42:17 -0400

    test

--- app/controllers/faddys_controller.rb
+++ app/controllers/faddys_controller.rb
@@ -30,4 +30,21 @@ class FaddysController < ApplicationController
     @tag_filter = ["dog", "sexy", "fashion", "swag", "funny", "gay"]
   end
 
+private
+
+  def find_results(location)
+    coordinates = Geocoder.coordinates("#{location}")
+  
+    x = coordinates[0]
+    y = coordinates[1]
+
+    loc = Instagram.media_search("#{x}","#{y}", options = {:count => 100})
+    loc_top = Instagram.media_search("#{x + 0.012}","#{y}", options = {:count => 100})
+    loc_bottom = Instagram.media_search("#{x - 0.012}","#{y}", options = {:count => 100})
+    loc_right = Instagram.media_search("#{x}","#{y - 0.012}", options = {:count => 100})
+    loc_left = Instagram.media_search("#{x}","#{y + 0.012}", options = {:count => 100})
+
+    results = loc + loc_top + loc_bottom + loc_left + loc_right
+  end
+
 end
