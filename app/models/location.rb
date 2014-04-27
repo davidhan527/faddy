def62ccd5209c3635393f4c73e5590902d0cb2f3
davidhan527 <davidhan527@gmail.com>
2014-04-27 15:38:30 -0400

    Subject Line: moving controller code to model What Happened: moving find results method to location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,21 @@ class Location < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
+
+  def find_results(location)
+    coordinates = Geocoder.coordinates("#{location}")
+  
+    x = coordinates[0]
+    y = coordinates[1]
+
+    loc = Instagram.media_search("#{x}","#{y}", options = {:count => 100})
+    loc_top = Instagram.media_search("#{x + 0.012}","#{y}", options = {:count => 100})
+    loc_bottom = Instagram.media_search("#{x - 0.012}","#{y}", options = {:count => 100})
+    loc_right = Instagram.media_search("#{x}","#{y - 0.012}", options = {:count => 100})
+    loc_left = Instagram.media_search("#{x}","#{y + 0.012}", options = {:count => 100})
+
+    @results = loc + loc_top + loc_bottom + loc_left + loc_right
+  end
+
+
 end
